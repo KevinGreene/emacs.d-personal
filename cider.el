344bf8545ec391f4eb04306abe6376fe714be9fe
Kevin Greene <kevin@spantree.net>
2016-04-12 12:45:14 -0400

    Updated emacs.d

--- cider.el
+++ cider.el
@@ -6,6 +6,16 @@
 (setq cider-auto-select-error-buffer nil)
 (setq cider-show-error-buffer nil)
 
+;; Save history to file
+(setq cider-repl-history-file "~/.cider_history")
+
 ;; Enables eldoc (minibuffer arg hints)
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (add-hook 'clojure-mode-hook #'cider-mode)
+
+;; Correct indentation for om.next
+(define-clojure-indent
+  (defui '(1 :form (:defn)))
+  (dom/div '(1 :form)))
+
+
--- preload/init.el
+++ preload/init.el
@@ -6,10 +6,25 @@
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 
-;;; Set Theme to be tomorrow
-;;; Code:
+
+(setq kevin-package-list '(ace-jump-buffer dash avy ace-jump-mode ace-window avy alchemist pkg-info epl company dash elixir-mode pkg-info epl align-cljlet clojure-mode anzu avy biblio biblio-core dash seq let-alist biblio-core dash seq let-alist browse-kill-ring cider-decompile javap-mode cider seq spinner queue pkg-info epl clojure-mode cider-profile cider seq spinner queue pkg-info epl clojure-mode coffee-mode color-theme-sanityinc-tomorrow company-anaconda s dash anaconda-mode f dash s s dash pythonic f dash s s dash company csv-mode deferred diff-hl diminish discover-my-major makey dockerfile-mode easy-kill elisp-slime-nav elixir-mode pkg-info epl exec-path-from-shell expand-region flx-ido flx flycheck seq let-alist pkg-info epl dash gist gh logito pcache git-timemachine gitconfig-mode gitignore-mode god-mode grizzl guru-mode handlebars-mode ido-ubiquitous ido-completing-read+ inf-ruby jade-mode javap-mode js2-refactor yasnippet s dash multiple-cursors s js2-mode json-mode json-snatcher json-reformat json-reformat json-rpc json-snatcher less-css-mode let-alist logito magit magit-popup dash async git-commit with-editor dash async dash with-editor dash async dash async magit-popup dash async makey markdown-mode monokai-theme move-text multiple-cursors mustache-mode neotree nrepl-sync cider seq spinner queue pkg-info epl clojure-mode operate-on-number ov pcache popup projectile pkg-info epl dash puppet-mode pkg-info epl python-x folding pythonic f dash s s dash queue rainbow-delimiters rainbow-mode ruby-tools rvm s sass-mode haml-mode scss-mode seq smart-mode-line rich-minority smartparens dash smartrep smex solarized-theme dash spinner undo-tree vkill volatile-highlights web-mode with-editor dash async yaml-mode yari yasnippet zenburn-theme zop-to-char))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+                                        ; install the missing packages
+(dolist (package kevin-package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;;; Set Theme
 (disable-theme 'zenburn)
-(setq prelude-theme 'sanityinc-tomorrow-night)
+(setq prelude-theme 'sanityinc-tomorrow-bright)
+;;(setq prelude-theme 'monokai)
+
 
 ;; Get rid of whitespace mode
 (setq prelude-whitespace nil)
@@ -35,5 +50,22 @@
 ;; Sets Neotree to F8
 (global-set-key [f8] 'neotree-toggle)
 
+;; Sets Alt + Space to delete all whitespace
+(global-set-key (kbd "s-SPC") 'just-one-space)
+
 ;; Turns on line numbers
 (global-linum-mode 1)
+
+;; Adds js2 refactor everytime we use js2
+;; (add-hook 'js2-mode-hook 'js2-jsx-mode)
+
+;; Automatically makes directories when visiting a file that doesn't exist
+;; Think mkdir -p
+(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir)))))
+
+
