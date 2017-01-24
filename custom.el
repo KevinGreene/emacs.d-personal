fadccf6cde1d1d8f0d2a7c386a3c20be5c719406
Kevin Greene <kevindgreene@gmail.com>
2017-01-24 10:06:49 -0500

    Lots of improvements to emacs

--- custom.el
+++ custom.el
@@ -6,8 +6,11 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" default)))
- '(js2-basic-offset 2)
- '(js2-strict-trailing-comma-warning nil))
+ '(js-indent-level 2)
+ '(js2-strict-trailing-comma-warning nil)
+ '(package-selected-packages
+   (quote
+    (kotlin-mode anti-zenburn-theme chef-mode zop-to-char zenburn-theme yari yaml-mode which-key web-mode volatile-highlights vkill use-package-chords undo-tree solarized-theme smex smartrep smartparens smart-mode-line scss-mode scala-mode2 sass-mode rvm ruby-tools rainbow-mode rainbow-delimiters python-x puppet-mode ov operate-on-number nrepl-sync neotree mustache-mode move-text monokai-theme markdown-mode magit less-css-mode json-rpc json-mode js2-refactor jade-mode inf-ruby imenu-anywhere ido-ubiquitous haskell-mode handlebars-mode guru-mode groovy-mode grizzl gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist flycheck-elm flycheck-elixir flx-ido expand-region exec-path-from-shell ensime elm-mode elisp-slime-nav easy-kill dockerfile-mode discover-my-major diff-hl deferred csv-mode crux company-go company-anaconda color-theme-sanityinc-tomorrow coffee-mode clj-refactor cider-profile cider-decompile browse-kill-ring biblio beacon anzu align-cljlet alchemist ace-window ace-jump-mode ace-jump-buffer))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- preload/init.el
+++ preload/init.el
@@ -6,6 +6,8 @@
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 
+(scroll-bar-mode -1)
+
 ;;; Set Theme
 (disable-theme 'zenburn)
 (setq prelude-theme 'sanityinc-tomorrow-bright)
@@ -31,6 +33,8 @@
   )
 (global-set-key "\C-c\C-d" 'duplicate-line)
 
+(global-set-key "\C-x\/" 'comment-region)
+
 ;; Sets Neotree teo F8
 (global-set-key [f8] 'neotree-toggle)
 
@@ -51,3 +55,38 @@
     (let ((dir (file-name-directory filename)))
       (unless (file-exists-p dir)
         (make-directory dir)))))
+
+;; Bigger font
+(set-face-attribute 'default nil :height 160)
+
+
+(defun my-personal-indents (n)
+  (setq indent-tabs-mode nil) ; use space instead of tab
+  (setq c-basic-offset n)
+  ;; web lang indents
+  (setq javascript-indent-level n) ; javascript-mode
+  (setq js-indent-level n) ; js-mode
+  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq web-mode-attr-indent-offset n)
+  (setq css-indent-offset n) ; css-mode
+  )
+
+(my-personal-indents 2)
+
+;; Increments a number, useful to silence vim critics
+;; As it's built into that platform
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
+(global-set-key (kbd "C-c +") 'increment-number-at-point)
+
+
+;; Set tab width to 4 (useful for golang)
+(setq tab-width 4)
